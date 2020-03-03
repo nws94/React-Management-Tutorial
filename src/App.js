e839f467c03f5de4bebe06f3bcc1b1a3cb5355a3
남우석 <hinyong22@gmail.com>
2020-03-03 19:58:05 +0900

    Create React Project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import Subject from './components/Subject'
+import TOC from './components/TOC'
+import Content from './components/Content'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  //constructor가 제일먼저 실행되고 초기화담당
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode:"read",
+      subject: {title:"WEB", sub:"world wide web"},
+      welcome: {title:"Welcome", desc:"Hello,React!!"},
+      contents:[
+        {id: 1, title:"HTML", desc:"HTML is for information"},
+        {id: 2, title:"CSS", desc:"CSS is for design"},
+        {id: 3, title:"javaScript", desc:"javaScrpit is for interactive"}
+      ]
+    }
+  }
+  render() {
+    let _title, _desc = null;
+    if(this.state.mode === "welcome"){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === "read") {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
+    return (
+      <div className="App">
+        <Subject
+        onClick={}
+        title={this.state.subject.title} 
+        sub={this.state.subject.sub}>
+        </Subject>
+        <TOC 
+          data={this.state.contents}>
+        </TOC>
+        <Content
+          title={_title}
+          desc={_desc}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </Content>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Content.js
@@ -0,0 +1,15 @@
+import React,{Component} from "react";
+
+class Content extends Component {
+  render() {
+    return(
+      <article>
+        <h2>{this.props.title}</h2>
+        <p>{this.props.desc}</p>
+      </article>
+    );
+  }
+}
+
+export default Content;
+
