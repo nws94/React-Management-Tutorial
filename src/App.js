349fd1f3f409905f30d44a34da47dc39c9f1823c
남우석 <hinyong22@gmail.com>
2020-03-03 23:54:39 +0900

    Apply Material-UI

--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import React from 'react';
 // import TOC from './components/TOC'
 // import Content from './components/Content'
 import Customer from './components/Customer'
+import Paper from '@material-ui/core/Paper'
+import Table from '@material-ui/core/Table'
+import TableHead from '@material-ui/core/TableHead'
+import TableBody from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
 import './App.css';
+import {withStyles} from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 class App extends React.Component {
   constructor(props) {
@@ -17,26 +35,39 @@ class App extends React.Component {
     }
   }
   render() {
-    
+    const { classes} = this.props;
     return (
-      <div className="App">
-        {
-          this.state.customers.map(c => {
-            return  <Customer
-                      key={c.id}
-                      id= {c.id}
-                      image = {c.image}
-                      name = {c.name}
-                      birthday = {c.birthday}
-                      gender = {c.gender}
-                      job = {c.job}
-                    />
-          })
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+              {
+                this.state.customers.map(c => {   
+                  return  <Customer
+                            key={c.id}
+                            id= {c.id}
+                            image = {c.image}
+                            name = {c.name}
+                            birthday = {c.birthday}
+                            gender = {c.gender}
+                            job = {c.job}
+                          />
+                })
         }
-       
-      </div>
+        </TableBody>
+       </Table>
+       </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
