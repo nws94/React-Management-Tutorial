3a43577fd1a93325d4bd516a60e2a3adfd57e52e
남우석 <hinyong22@gmail.com>
2020-03-03 22:54:22 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,39 @@
-import React,{Component} from 'react';
-import Subject from './components/Subject'
-import TOC from './components/TOC'
-import Content from './components/Content'
+import React from 'react';
+// import Subject from './components/Subject'
+// import TOC from './components/TOC'
+// import Content from './components/Content'
+import Customer from './components/Customer'
 import './App.css';
 
-class App extends Component {
-  //constructor가 제일먼저 실행되고 초기화담당
+class App extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      mode:"read",
-      subject: {title:"WEB", sub:"world wide web"},
-      welcome: {title:"Welcome", desc:"Hello,React!!"},
-      contents:[
-        {id: 1, title:"HTML", desc:"HTML is for information"},
-        {id: 2, title:"CSS", desc:"CSS is for design"},
-        {id: 3, title:"javaScript", desc:"javaScrpit is for interactive"}
+      customers: [
+        {id: 1,image: "https://placeimg.com/64/64/1",name: "남우석", birthday: "941008", gender: "남자", job: "대학생"},
+        {id: 2,image: "https://placeimg.com/64/64/2",name: "홍길동", birthday: "940504", gender: "여자", job: "백수"},
+        {id: 3,image: "https://placeimg.com/64/64/3",name: "김나영", birthday: "940313", gender: "남자", job: "회사원"},
       ]
     }
   }
   render() {
-    let _title, _desc = null;
-    if(this.state.mode === "welcome"){
-      _title = this.state.welcome.title;
-      _desc = this.state.welcome.desc;
-    } else if(this.state.mode === "read") {
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
-    }
+    
     return (
       <div className="App">
-        <Subject
-        onClick={}
-        title={this.state.subject.title} 
-        sub={this.state.subject.sub}>
-        </Subject>
-        <TOC 
-          data={this.state.contents}>
-        </TOC>
-        <Content
-          title={_title}
-          desc={_desc}
-        >
-        </Content>
+        {
+          this.state.customers.map(c => {
+            return  <Customer
+                      key={c.id}
+                      id= {c.id}
+                      image = {c.image}
+                      name = {c.name}
+                      birthday = {c.birthday}
+                      gender = {c.gender}
+                      job = {c.job}
+                    />
+          })
+        }
+       
       </div>
     );
   }
