4f4a4dab85cc7d1e89f2c97d71ce2074131b3938
남우석 <hinyong22@gmail.com>
2020-03-13 22:22:19 +0900

    고객 추가 양식 구현 및 이벤트 핸들링 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
-// import Subject from './components/Subject'
-// import TOC from './components/TOC'
-// import Content from './components/Content'
 import Customer from './components/Customer'
+import CustomerAdd from "./components/CustomerAdd"
 import Paper from '@material-ui/core/Paper'
 import Table from '@material-ui/core/Table'
 import TableHead from '@material-ui/core/TableHead'
@@ -11,7 +9,7 @@ import TableRow from '@material-ui/core/TableRow'
 import TableCell from '@material-ui/core/TableCell'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import './App.css';
-import {withStyles} from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles'
 
 const styles = theme => ({
   root: {
@@ -53,40 +51,43 @@ class App extends React.Component {
   render() {
     const { classes} = this.props;
     return (
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-              {
-                this.state.customers ? this.state.customers.map(c => {   
-                                        return  <Customer
-                                                  key={c.id}
-                                                  id= {c.id}
-                                                  image = {c.image}
-                                                  name = {c.name}
-                                                  birthday = {c.birthday}
-                                                  gender = {c.gender}
-                                                  job = {c.job}
-                                                />
-                                      }) : <TableRow>
-                                            <TableCell colSpan="6" align="center">
-                                              <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
-                                            </TableCell>
-                                          </TableRow>
-          }
-        
-        </TableBody>
-       </Table>
-       </Paper>
+      <div>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+                {
+                  this.state.customers ? this.state.customers.map(c => {   
+                                          return  <Customer
+                                                    key={c.id}
+                                                    id= {c.id}
+                                                    image = {c.image}
+                                                    name = {c.name}
+                                                    birthday = {c.birthday}
+                                                    gender = {c.gender}
+                                                    job = {c.job}
+                                                  />
+                                        }) : <TableRow>
+                                              <TableCell colSpan="6" align="center">
+                                                <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+                                              </TableCell>
+                                            </TableRow>
+            }
+          
+          </TableBody>
+        </Table>
+        </Paper>
+        <CustomerAdd/>
+       </div>
     );
   }
 }
